d58b3185ecf166e39e882e43081878f6498c0618
sagar <girisagar0014@gmail.com>
2022-07-22 14:57:17 +0545

    google site

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,17 @@ import React from 'react';
 import WithLayout from '@components/WithLayout';
 import LandingPage from '@views/ViewProduct';
 import { Main } from 'src/layouts';
+import Head from 'next/head';
 
 const Index: NextPage = () => {
   return (
     <>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="bJikpTy5BTn5u9snTb7hZfh8UQXR0invGtY7K0dyObU"
+        />
+      </Head>
       <WithLayout component={LandingPage} layout={Main} />
     </>
   );
