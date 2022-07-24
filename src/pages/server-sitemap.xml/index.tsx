f32323c9b949cc4853675d5acdeed816b7998fd2
sagar <girisagar0014@gmail.com>
2022-07-24 18:44:11 +0545

    extra slug

--- src/pages/server-sitemap.xml/index.tsx
+++ src/pages/server-sitemap.xml/index.tsx
@@ -1,8 +1,23 @@
 import { GetServerSideProps } from 'next';
 import { getServerSideSitemap, ISitemapField } from 'next-sitemap';
-
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  let slugs = ['facebook', 'facebook-view', 'snapchat'];
+  
+  let slugs = ['facebook', 'facebook-view', 'snapchat',
+'aviary-for-twitter',
+'secretmanager',
+'secretpath-find-way-home',
+'secretstring',
+'secrettext',
+'secretvault-pro-lock-photos',
+'secreta-beauty-lab',
+'secretalk',
+'secretari',
+'secretaria-es',
+'secretary-run',
+'secretaria-de-cultura',
+'secretaria-de-educacion-tamaulipas'
+
+];
   const fields: ISitemapField[] = slugs.map((item) => ({
     loc: `https://demo-pi-two.vercel.app/app/${item}`
   }));
